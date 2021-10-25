d233f1c73fab6b77d784ca72bf2fa8de863af9e4
Matt Holmes <m4ttholmes@gmail.com>
2021-10-25 14:23:13 -0400

    installs

---
+++ 24hr_project/src/location.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+function componentDidMount() {
+    if ("geolocation" in navigator) {
+      console.log("Available");
+    } else {
+        console.log("Not Available");
+    }
+}
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     };
+//   }
+
+//   componentDidMount() {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       console.log("Latitude is :", position.coords.latitude);
+//       console.log("Longitude is :", position.coords.longitude);
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h4>Using geolocation JavaScript API in React</h4>
+//       </div>
+//     );
+//   }
+// }
+
+
+render(<App />, document.getElementById("root"));
