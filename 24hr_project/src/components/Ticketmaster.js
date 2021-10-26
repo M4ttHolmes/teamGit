69cc5eaab5edcbe34ecaf8f8bae619777847ae91
Riley Roush <riley.roush@gmail.com>
2021-10-25 21:38:32 -0400

    ticketmaster added

--- 24hr_project/src/components/Ticketmaster.js
+++ 24hr_project/src/components/Ticketmaster.js
@@ -1,39 +1,45 @@
 import React from 'react';
-
+import Location from './GeoLocation';
 
 
 class Ticketmaster extends React.Component {
+
     constructor(props) {
         super(props)
         this.state = {
-            baseUrl: 'https://app.ticketmaster.com/discovery/v2/suggest/geoPoint',
-            apiKey: 'eCqUZLBcgktCoAWOpA2UA8P5TYnsFwjI',
-            location: {},
-            venue: {},
+            url: 'api.openweathermap.org/data/2.5/weather?',
+            key: 'eCqUZLBcgktCoAWOpA2UA8P5TYnsFwjI',
+            lat: '',
+            long: ''
         }
     }
 
-    componentDidMount() {
 
-        fetch(`$this.state.baseURL`)
-            .then(res => res.json())
-            .then(json => {
-                this.setState({
-                    isLoaded: true,
-                    items: json
-                })
-            });
+componentDidMount = () => {
+    console.log("Ticketmaster")
+    fetch(`${this.state.url}lat=${this.props.lat}&lon=${this.props.long}&appid=${this.state.key}`)
+    
+    .then(response => console.log(response))
+    //response.json())
 
-    }
+    //.then(json => {
+        //console.log(response)
+    .catch(err => console.log(err))
+    //})
+   
+};
 
-    render() {      
-        return (
-            <div className='main'>              
-                <div className='mainDiv' style={{textAlign: 'center'}}></div>
-            </div>
-        )
-    }
+render() {
+    return (
+        <div className='main'>
+            <ul id='event-name'>
+            </ul>
+            <br />
+        </div>
+    )
+      
+    
 }
-
+}
 
 export default Ticketmaster;
